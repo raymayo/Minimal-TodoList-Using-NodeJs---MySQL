5c8b3a954b21ba1062671fb4de5b42471d315c33
Ray Mayo <ormillada.jr@gmail.com>
2023-01-26 17:23:24 +0800

    changes

--- script.js
+++ script.js
@@ -1,115 +1,101 @@
-
-
 const textField = document.querySelector('#inputValue');
 const submitBtn = document.querySelector('#submitBtn');
 const todoContainer = document.querySelector('#todoContainer');
-const clearAllBtn = document.querySelector('#clearAllBtn')
 
 let deleteTable = document.getElementsByClassName('delete');
 
 let todoData;
 
 
-submitBtn.addEventListener('click', ()=>{
-    displayText(textField.value);
-
-    if(textField.value === ''){
-        return;
-    }
-    todoData = { todo: textField.value };
-
-
-
-    fetch('http://localhost:3555/post', {
-        method: 'POST',
-        body: JSON.stringify(todoData),
-        headers: { 'Content-Type': 'application/json' }
-    })
-    .then(res => res.text())
-    .then(response => console.log('Success:', response))
-    .catch(error => console.error('Error:', error));
-
-    textField.value = '';
-})
-
-
-function displayText(inputValue, inputKey){
+function displayText(inputValue, inputKey) {
     let todoBox = document.createElement('div');
     todoBox.className = 'flex justify-between w-full gap-2';
 
     let textBox = document.createElement('p');
-    textBox.className = 'border py-1.5 px-3 w-full rounded border-gray-500 text-gray-800';
+    textBox.className =
+        'border py-1.5 px-3 w-full rounded border-gray-500 text-gray-800';
     textBox.textContent = inputValue;
 
     let deleteBtn = document.createElement('button');
-    deleteBtn.className = 'px-3 text-white bg-gray-600 rounded delete cursor-pointer';
+    deleteBtn.className =
+        'px-3 text-white bg-gray-600 rounded delete cursor-pointer';
     deleteBtn.textContent = 'X';
 
-    todoBox.setAttribute('dataKey', inputKey)
+    todoBox.setAttribute('dataKey', inputKey);
 
-
-
-
-
-    todoBox.appendChild(textBox)
+    todoBox.appendChild(textBox);
     todoBox.appendChild(deleteBtn);
 
     todoContainer.appendChild(todoBox);
-
 }
 
+submitBtn.addEventListener('click', () => {
+	if (textField.value === '') {
+		return;
+	}
+
+    displayText(textField.value);
+
+	todoData = { todo: textField.value };
+
+	fetch('http://localhost:3555/post', {
+		method: 'POST',
+		body: JSON.stringify(todoData),
+		headers: { 'Content-Type': 'application/json' },
+	})
+		.then((res) => res.text())
+		.then((response) => console.log('Success:', response))
+		.catch((error) => console.error('Error:', error));
+
+	textField.value = '';
+});
 
 
 
-function displayTodos(){
-    fetch('http://localhost:3333/todos')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(e => {
-                displayText(e.id, e.id)
-            });
-            deleteData();
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+function displayTodos() {
+	fetch('http://localhost:3333/todos')
+		.then((response) => response.json())
+		.then((data) => {
+			data.forEach((e) => {
+				displayText(e.todos, e.id);
+			});
+			deleteData();
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
 }
 
 displayTodos();
 
+function deleteData() {
+	for (const deleteBtn of deleteTable) {
+		deleteBtn.addEventListener('click', () => {
+			fetch('http://localhost:3333/todos')
+				.then((response) => response.json())
+				.then((data) => {
+					data.forEach((e) => {
+						if (e.id == deleteBtn.parentElement.getAttribute('dataKey')) {
+							let deleteData = { todo: e.id };
 
+							fetch('http://localhost:3444/delete', {
+								method: 'POST',
+								body: JSON.stringify(deleteData),
+								headers: { 'Content-Type': 'application/json' },
+							})
+								.then((res) => res.text())
+								.then((response) => console.log('Data Deleted:', response))
+								.catch((error) => console.error('Error:', error));
 
-function deleteData(){
-    for (const deleteBtn of deleteTable) {
-        deleteBtn.addEventListener('click',()=>{
-            fetch('http://localhost:3333/todos')
-                .then(response => response.json())
-                .then(data => {
-                    data.forEach(e => {
-                        if (e.id == deleteBtn.parentElement.getAttribute('dataKey')){
-    
-                            let deleteData = { todo: e.id };
-
-                            console.log(deleteData)
-
-                            fetch('http://localhost:3444/delete', {
-                                method: 'POST',
-                                body: JSON.stringify(deleteData),
-                                headers: { 'Content-Type': 'application/json' }
-                            })
-                                .then(res => res.text())
-                                .then(response => console.log('Data Deleted:', response))
-                                .catch(error => console.error('Error:', error));
-
-                                deleteBtn.parentElement.remove()
-                       }
-                    });
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-        })
-    }
+							deleteBtn.parentElement.remove();
+						}
+					});
+				})
+				.catch((error) => {
+					console.error('Error:', error);
+				});
+		});
+	}
 }
 
 deleteData();
